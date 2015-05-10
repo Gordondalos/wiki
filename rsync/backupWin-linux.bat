0712ce2927b7fc09a379d2690a1ac1cdd41b4bdc
gordondalos <bondarenko.kg@mail.ru>
2015-05-10 22:30:31 +0600

    no message

---
+++ rsync/backupWin-linux.bat
@@ -0,0 +1,2 @@
+c:\cygwin\bin\rsync.exe -rt -v  "/cygdrive/c/1" gordondalos@192.168.0.103:/home/gordondalos/mybackup
+pause
---
+++ rsync/backup_Windows.bat
@@ -0,0 +1,2 @@
+c:\cygwin\bin\rsync.exe -rt --verbose  "/cygdrive/c/1" '/cygdrive/c/Users/gordondalos/Desktop/mybackup'
+pause
